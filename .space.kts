4f7ed0dfd006e510e5889893319f949ea064798b
virusbear <fabianfahrenholz718@gmail.com>
2023-09-10 11:51:52 +0200

    Update Build Pipeline

--- .space.kts
+++
@@ -1,9 +0,0 @@
-/**
-* JetBrains Space Automation
-* This Kotlin script file lets you automate build activities
-* For more info, see https://www.jetbrains.com/help/space/automation.html
-*/
-
-job("Build") {
-	gradlew("amazoncorretto:17-alpine", "clean build")
-}
