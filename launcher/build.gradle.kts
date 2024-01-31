a1541e231d5589942bd0ef794d987de39e2ec009
virusbear <fabianfahrenholz718@gmail.com>
2024-01-31 19:50:23 +0100

    Remove old beanstalk tests and implement mayan api
    Removed beanstalkt-operation tests due to refactoring of functions. This was necessary to start separation of the BEANSTALKD from the main application. In addition, implemented the initial structure for the mayan API client which is crucial for interaction with the external service.

--- launcher/build.gradle.kts
+++ launcher/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    application
 }
 
 val coroutines_version: String by project
@@ -17,7 +18,7 @@ dependencies {
     implementation(project(":entrypoint"))
     implementation(project(":worker"))
     implementation(project(":mayan-client"))
-    implementation(project(":beanstalkd-client"))
+    implementation("com.virusbear:beanstalkt:1.1.0")
 }
 
 application {
--- mayan-client/build.gradle.kts
+++ mayan-client/build.gradle.kts
@@ -1,23 +1,13 @@
 plugins {
     kotlin("jvm")
-    kotlin("plugin.serialization") version "1.9.0"
 }
 
-val kotlin_version: String by project
 val coroutines_version: String by project
-val serialization_version = "1.3.3"
-val ktor_version: String by project
 val logging_version: String by project
 
 dependencies {
     implementation("io.github.microutils:kotlin-logging:$logging_version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serialization_version")
 
-    api("io.ktor:ktor-client-core:$ktor_version")
-    api("io.ktor:ktor-client-serialization:$ktor_version")
-    api("io.ktor:ktor-client-content-negotiation:$ktor_version")
-    api("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-
-    implementation("io.ktor:ktor-client-cio-jvm:$ktor_version")
+    api(project(":mayan-api"))
 }
