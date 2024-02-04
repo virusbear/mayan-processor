989825857765d47f82a280a9e3f6305797cd3f6c
virusbear <fabianfahrenholz718@gmail.com>
2024-02-04 15:05:12 +0100

    Add SmartLinks model class
    This commit introduces the SmartLinks model class in the mayan-client package. It wraps the API response for smart links and offers a cleaner and more idiomatic way to handle data. This change supports better, more structured access to smart link properties.

---
+++ mayan-client/src/main/kotlin/com/virusbear/mayan/client/model/SmartLinks.kt
@@ -0,0 +1,46 @@
+package com.virusbear.mayan.client.model
+
+import com.virusbear.mayan.api.client.model.ApiSmartLink
+import java.net.URI
+
+class SmartLinks(
+    private val api: ApiSmartLink
+) {
+    //region fields
+    val label: String
+        get() = api.label
+
+    val conditionsUrl: URI
+        get() = api.conditionsUrl!!
+
+    val documentTypesUrl: URI
+        get() = api.documentTypesUrl!!
+
+    val documentTypesAddUrl: URI
+        get() = api.documentTypesAddUrl!!
+
+    val documentTypesRemoveUrl: URI
+        get() = api.documentTypesRemoveUrl!!
+
+    val dynamicLabel: String
+        get() = api.dynamicLabel!!
+
+    val enabled: Boolean
+        get() = api.enabled!!
+
+    val id: Int
+        get() = api.id!!
+
+    val url: URI
+        get() = api.url!!
+    //endregion
+
+    //region navigate_multiple
+    //endregion
+
+    //region navigate_single
+    //endregion
+
+    //region operations
+    //endregion
+}
