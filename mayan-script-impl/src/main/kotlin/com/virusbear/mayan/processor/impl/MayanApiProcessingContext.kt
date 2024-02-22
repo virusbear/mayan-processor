0cf1568af91fc39814a55f35ce4d767b6c804974
virusbear <fabianfahrenhlz718@gmail.com>
2024-02-22 20:19:06 +0100

    Minor update to MayanProcessorHost

--- mayan-script-impl/src/main/kotlin/com/virusbear/mayan/processor/impl/MayanApiProcessingContext.kt
+++ mayan-script-impl/src/main/kotlin/com/virusbear/mayan/processor/impl/MayanApiProcessingContext.kt
@@ -1,17 +1,14 @@
 package com.virusbear.mayan.processor.impl
 
-import com.virusbear.mayan.client.MayanClient
 import com.virusbear.mayan.processor.Document
 import com.virusbear.mayan.processor.ProcessingContext
-import kotlinx.coroutines.runBlocking
 import com.virusbear.mayan.client.model.Document as ApiDocument
 
 class MayanApiProcessingContext(
-    private val client: MayanClient,
     private val doc: ApiDocument
 ): ProcessingContext {
     override val document: Document
-        get() = MayanDocument(client, doc)
+        get() = MayanDocument(doc)
 
     override suspend fun regex(pattern: String, group: Int): String =
         pattern.toRegex().find(document.content())?.groups?.get(group)?.value ?: ""
@@ -24,11 +21,7 @@ class MayanApiProcessingContext(
     }
 
     override suspend fun metadata(type: String, value: String) {
-        runBlocking {
-            client.metadataIdForDocument(document.id(), type)?.let {
-                client.metadata(document.id(), it, value)
-            }
-        }
+        TODO("Not yet supported")
     }
 
     override suspend fun documentType(type: String) {
--- mayan-script-impl/src/main/kotlin/com/virusbear/mayan/processor/impl/MayanDocument.kt
+++ mayan-script-impl/src/main/kotlin/com/virusbear/mayan/processor/impl/MayanDocument.kt
@@ -1,6 +1,5 @@
 package com.virusbear.mayan.processor.impl
 
-import com.virusbear.mayan.client.MayanClient
 import com.virusbear.mayan.processor.Document
 import com.virusbear.mayan.processor.Page
 import kotlinx.coroutines.flow.filter
@@ -8,11 +7,10 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.flow.toSet
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import kotlin.reflect.KMutableProperty
 import com.virusbear.mayan.client.model.Document as ApiDocument
 
 class MayanDocument(
-    private val client: MayanClient,
     private val document: ApiDocument
 ): Document {
     private var _indices: Set<String>? = null
@@ -24,15 +22,15 @@ class MayanDocument(
     override suspend fun id(): Int =
         document.id
     override suspend fun name(): String =
-        document.label ?: ""
+        document.label
     override suspend fun filename(): String =
-        document.fileLatest?.filename ?: ""
+        document.latestFile.filename
     override suspend fun filesize(): Int =
-        document.fileLatest?.propertySize ?: -1
+        document.latestFile.size
     override suspend fun timestamp(): LocalDateTime =
-        document.datetimeCreated?.let { LocalDateTime.parse(it, DateTimeFormatter.ISO_LOCAL_DATE_TIME) } ?: LocalDateTime.MIN
+        document.dateTimeCreated.toLocalDateTime()
     override suspend fun type(): String =
-        document.documentType?.label ?: ""
+        document.type.label
     override suspend fun content(): String =
         pages().joinToString("\n") { it.content }
 
@@ -49,9 +47,13 @@ class MayanDocument(
         _pages ?: client.pages(document.id).map {
             MayanDocumentPage(client, document.id, it)
         }.toList().sortedBy { it.pageNumber }.also { _pages = it }
+
     override suspend fun mimetype(): String =
-        document.fileLatest?.mimetype ?: ""
+        document.latestFile.mimetype
 
     override suspend fun file(): ByteArray =
         _file ?: client.fileContent(document.id, document.fileLatest?.id!!).also { _file = it }
+
+    private infix inline fun <T> KMutableProperty<T?>.providedBy(block: () -> T): T =
+        getter.call() ?: block().also { setter.call(it) }
 }
